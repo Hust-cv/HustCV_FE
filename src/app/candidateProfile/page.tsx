81bc3f7bad935dd800a261c380b00c8fd495a07e
IMnhvjp <levanminh19102003@gmail.com>
2023-12-21 17:17:10 +0700

    update education

--- src/app/candidateProfile/page.tsx
+++ src/app/candidateProfile/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React, { useState } from 'react';
+import dayjs from 'dayjs';
 import {
     Button,
     Modal,
     DatePicker,
     Form,
     Input,
+    Checkbox,
     Radio,
     Select,
     message,
@@ -17,7 +19,7 @@ import {
 } from 'antd';
 import { MoreOutlined, CheckOutlined, FireOutlined, ReadOutlined, FormOutlined, TagOutlined, EditOutlined, DeleteOutlined, UserOutlined, HomeOutlined, MoneyCollectOutlined, FieldTimeOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import type { SizeType } from 'antd/es/config-provider/SizeContext';
-import type { SelectProps } from 'antd';
+import type { SelectProps, DatePickerProps } from 'antd';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { IPost } from '../shared/recruitmentPost.type';
 import http from '../utils/http';
@@ -35,15 +37,12 @@ const candidateProfile = () => {
     const [deleteEducation, setDeleteEducation] = useState(null)
 
     const queryClient = useQueryClient()
-
     const education = useQuery({
         queryKey: ['education'],
         queryFn: async () => {
-            const values = {
-                personalFileId : 1
-            }
+            const userId = await localStorage.getItem('userId');
             try {
-                const response = await http.axiosClient.post('/api/profile/education/getAll', values)
+                const response = await http.axiosClient.post('/api/profile/education/getAll', {personalFileId: userId})
                 return response.data
             } catch (error) {
 
@@ -53,12 +52,15 @@ const candidateProfile = () => {
 
     const addNewEducationMutation = useMutation({
         mutationFn: async (values: any) => {
-            const data = await http.axiosClient.post('/api/profile/education', values)
+            const userId = await localStorage.getItem('userId');
+            values.start = values.start.format('MM/YYYY');
+            values.end = values.end.format('MM/YYYY');
+            const name = values.schools + '*/' + values.majors + '*/' + values.start + ' - ' + (values.isStillStudy == true ? 'Hiện tại' : values.end)
+            const data = await http.axiosClient.post('/api/profile/education', {name: name, personalFileId: userId})
             return data
         },
         onSuccess: (data, variables, context) => {
             message.success('Thêm học vấn thành công!')
-            // setIsModalOpen(false)
             queryClient.invalidateQueries({ queryKey: ['education'] })
         },
         onError: (error: any) => {
@@ -72,7 +74,11 @@ const candidateProfile = () => {
     const cancelModalAddEdu = () => {
         setModalAddEduOpen(false);
     }
+    const onChangeCheckBoxEdu = () => {
+
+    }
     const finishAddEdu = (values: any) => {
+        setModalAddEduOpen(false);
         addNewEducationMutation.mutate(values)
     }
 
@@ -88,13 +94,63 @@ const candidateProfile = () => {
         return(
             <>
                 <div className='mx-[300px]'>
-                    <Modal title="Basic Modal"
+                    <Modal title="Học vấn"
                         open={isModalAddEduOpen}
                         onCancel={cancelModalAddEdu}
+                        footer={[
+                        ]}
                     >
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
-                        <p>Some contents...</p>
+                        <Form
+                            name="basic"
+                            labelCol={{ span: 8 }}
+                            wrapperCol={{ span: 16 }}
+                            style={{ maxWidth: 600 }}
+                            initialValues={{ isStillStudy: false }}
+                            onFinish={finishAddEdu}
+                        >
+                            <Form.Item
+                                label="Trường"
+                                name="schools"
+                                rules={[{ required: true, message: 'Vui lòng nhập tên trường của bạn!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Ngành học"
+                                name="majors"
+                                rules={[{ required: true, message: 'Vui lòng nhập tên ngành của bạn!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                name="isStillStudy"
+                                valuePropName='checked'
+                                wrapperCol={{ offset: 8, span: 16 }}
+                            >
+                                <Checkbox onChange={onChangeCheckBoxEdu} >Đang học</Checkbox>
+                            </Form.Item>
+                            <Form.Item
+                                label="Từ"
+                                name="start"
+                                rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
+                            >
+                                <DatePicker picker="month" />
+                            </Form.Item>
+                            <Form.Item
+                                label="Đến"
+                                name="end"
+                                rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
+                            >
+                                <DatePicker picker="month" />
+                            </Form.Item>
+                            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                                <Button type="primary" htmlType="submit" className='bg-blue-600'>
+                                    Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
                     </Modal>
                     <Card
                         title={
