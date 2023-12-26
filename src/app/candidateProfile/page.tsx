300b435aeb18671fafe600d2956a0beceabd47aa
IMnhvjp <levanminh19102003@gmail.com>
2023-12-26 10:17:57 +0700

    update edu

--- src/app/candidateProfile/page.tsx
+++ src/app/candidateProfile/page.tsx
@@ -34,7 +34,10 @@ const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
 const candidateProfile = () => {
     const [isModalAddEduOpen,setModalAddEduOpen] = useState(false)
     const [isModalEditEduOpen, setModalEditEduOpen] = useState(false)
-    const [deleteEducation, setDeleteEducation] = useState(null)
+    const [isModalConfirmDeleteEduOpen, setModalConfirmDeleteEduOpen] = useState(false)
+    const [disabledCheckboxEdu, setDisabledCheckboxEdu] = useState(false)
+    const [deleteEdu, setDeleteEdu] = useState(null)
+    const [editEdu, setEditEdu] = useState<any>({})
 
     const queryClient = useQueryClient()
     const education = useQuery({
@@ -50,18 +53,49 @@ const candidateProfile = () => {
         }
     })
 
-    const addNewEducationMutation = useMutation({
+    const addNewEduMutation = useMutation({
         mutationFn: async (values: any) => {
             const userId = await localStorage.getItem('userId');
-            values.start = values.start.format('MM/YYYY');
-            values.end = values.end.format('MM/YYYY');
-            const name = values.schools + '*/' + values.majors + '*/' + values.start + ' - ' + (values.isStillStudy == true ? 'Hiện tại' : values.end)
-            const data = await http.axiosClient.post('/api/profile/education', {name: name, personalFileId: userId})
+            values.start = values.start.toISOString();
+            values.end = values.end.toISOString();
+            const name = values.schools + '*/' + values.majors + '*/' + values.start + '*/' + (values.isStillStudy == true ? 'Hiện tại' : values.end)
+            const data = await http.axiosClient.post('/api/profile/education/add', {name: name, personalFileId: userId})
             return data
         },
         onSuccess: (data, variables, context) => {
             message.success('Thêm học vấn thành công!')
             queryClient.invalidateQueries({ queryKey: ['education'] })
+        },
+        onError: (error: any) => {
+            message.error(error.response.data.message)
+        }
+    })
+
+    const deleteEduMutation = useMutation({
+        mutationFn: async (id: any) => {
+            const response = await http.axiosClient.delete('/api/profile/education/' + id)
+            return response
+        },
+        onSuccess: (data, variables, context) => {
+            message.success('Xóa học vấn thành công!')
+            queryClient.invalidateQueries({ queryKey: ['education']})
+        },
+        onError: (error: any) => {
+            message.error(error.response.data.message)
+        }
+    })
+
+    const editEduMutation = useMutation({
+        mutationFn: async ({ id, values }: any) => {
+            values.start = values.start.toISOString();
+            values.end = values.end.toISOString();
+            const name = values.schools + '*/' + values.majors + '*/' + values.start + '*/' + (values.isStillStudy == true ? 'Hiện tại' : values.end)
+            const response = await http.axiosClient.put('/api/profile/education/' + id, {name: name})
+            return response
+        },
+        onSuccess: (data, variables, context) => {
+            message.success('Cập nhật học vấn thành công!')
+            queryClient.invalidateQueries({ queryKey: ['education']})
         },
         onError: (error: any) => {
             message.error(error.response.data.message)
@@ -74,121 +108,219 @@ const candidateProfile = () => {
     const cancelModalAddEdu = () => {
         setModalAddEduOpen(false);
     }
-    const onChangeCheckBoxEdu = () => {
-
+    const onChangeCheckBoxEdu = (checked: any) => {
+        // setDisabledCheckboxEdu((checked))
     }
     const finishAddEdu = (values: any) => {
         setModalAddEduOpen(false);
-        addNewEducationMutation.mutate(values)
+        addNewEduMutation.mutate(values);
     }
 
-    const showModalEditEdu = (post: any) => {
-
+    const cancelModalConfirmDelete = () => {
+        setDeleteEdu(null)
+        setModalConfirmDeleteEduOpen(false);
     }
 
-    const handleDeleteEdu = (id: any) => {
+    const handleDeleteEdu = (idEdu: any) => {
+        setDeleteEdu(idEdu)
+        setModalConfirmDeleteEduOpen(true);
+    }
+    const finishDeleteEdu = () => {
+        deleteEduMutation.mutate(deleteEdu);
+        cancelModalConfirmDelete();
+    }
+    const cancelModalEditEdu = () => {
+        setModalEditEduOpen(false);
+    }
 
+    const handleEditEdu = (infoEdu: any) => {
+        setEditEdu(infoEdu);
+        setModalEditEduOpen(true);
+    }
+
+    const finishEditEdu = (values: any) => {
+        let id = editEdu.id
+        editEduMutation.mutate({id, values});
+        cancelModalEditEdu();
     }
 
     const profile1 = () => {
         return(
             <>
                 <div className='mx-[300px]'>
-                    <Modal title="Học vấn"
-                        open={isModalAddEduOpen}
-                        onCancel={cancelModalAddEdu}
-                        footer={[
-                        ]}
-                    >
-                        <Form
-                            name="basic"
-                            labelCol={{ span: 8 }}
-                            wrapperCol={{ span: 16 }}
-                            style={{ maxWidth: 600 }}
-                            initialValues={{ isStillStudy: false }}
-                            onFinish={finishAddEdu}
+                    <div>
+                        <Modal title="Thêm học vấn"
+                            open={isModalAddEduOpen}
+                            onCancel={cancelModalAddEdu}
+                            footer={null}
                         >
-                            <Form.Item
-                                label="Trường"
-                                name="schools"
-                                rules={[{ required: true, message: 'Vui lòng nhập tên trường của bạn!' }]}
+                            <Form
+                                name="basic"
+                                labelCol={{ span: 8 }}
+                                wrapperCol={{ span: 16 }}
+                                style={{ maxWidth: 600 }}
+                                initialValues={{ isStillStudy: false }}
+                                onFinish={finishAddEdu}
                             >
-                                <Input />
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Ngành học"
-                                name="majors"
-                                rules={[{ required: true, message: 'Vui lòng nhập tên ngành của bạn!' }]}
-                            >
-                                <Input />
-                            </Form.Item>
-
-                            <Form.Item
-                                name="isStillStudy"
-                                valuePropName='checked'
-                                wrapperCol={{ offset: 8, span: 16 }}
-                            >
-                                <Checkbox onChange={onChangeCheckBoxEdu} >Đang học</Checkbox>
-                            </Form.Item>
-                            <Form.Item
-                                label="Từ"
-                                name="start"
-                                rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
-                            >
-                                <DatePicker picker="month" />
-                            </Form.Item>
-                            <Form.Item
-                                label="Đến"
-                                name="end"
-                                rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
-                            >
-                                <DatePicker picker="month" />
-                            </Form.Item>
-                            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                                <Button type="primary" htmlType="submit" className='bg-blue-600'>
-                                    Submit
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </Modal>
-                    <Card
-                        title={
-                            <>
-                                <p className='font-bold text-3xl'>Học vấn</p>
-                            </>
-                        }
-                        extra={
-                            <>
-                                <div className='mb-4 mt-2'>
-                                    <a onClick={() => showModalAddEdu()}><PlusCircleOutlined className='mr-4 text-2xl' /></a>
-                                </div>
-                            </>
-                        }
-                        className='w-full mb-4 border-black'
-                    >
-                        {education?.data?.map((info: any) => {
-                            return (
-                                <Card
-                                    key={info.id}
-                                    title={
-                                        <>
-                                            <p className='font-bold text-xl'>{info.name.split('*/')[0]}</p>
-                                        </>
-                                    }
-                                    extra={
-                                        <>
-                                            <a onClick={() => showModalEditEdu(info.id)}><EditOutlined className='mr-8' /></a>
-                                            <a onClick={() => handleDeleteEdu(info.id)}><DeleteOutlined className='' /></a>
-                                        </>
-                                    }
+                                <Form.Item
+                                    label="Trường"
+                                    name="schools"
+                                    rules={[{ required: true, message: 'Vui lòng nhập tên trường của bạn!' }]}
                                 >
-                                    <p className='my-3 text-base'>{info.name.split('*/')[1]}</p>
-                                    <p className='my-3 text-base'>{info.name.split('*/')[2]}</p>
-                                </Card>
-                            )
-                        })}
-                    </Card>
+                                    <Input />
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Ngành học"
+                                    name="majors"
+                                    rules={[{ required: true, message: 'Vui lòng nhập tên ngành của bạn!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+
+                                <Form.Item
+                                    name="isStillStudy"
+                                    valuePropName='checked'
+                                    wrapperCol={{ offset: 8, span: 16 }}
+                                >
+                                    <Checkbox onChange={(e: any) => {onChangeCheckBoxEdu(e.target.checked)}} >Đang học</Checkbox>
+                                </Form.Item>
+                                <Form.Item
+                                    label="Từ"
+                                    name="start"
+                                    rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
+                                >
+                                    <DatePicker picker="month" />
+                                </Form.Item>
+                                <Form.Item
+                                    label="Đến"
+                                    name="end"
+                                    rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
+                                >
+                                    <DatePicker picker="month" disabled={disabledCheckboxEdu} />
+                                </Form.Item>
+                                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                                    <Button type="primary" htmlType="submit" className='bg-blue-600'>
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Modal>
+                    </div>
+                    <div>
+                        <Modal title="Chú ý" 
+                            open={isModalConfirmDeleteEduOpen}
+                            onCancel={cancelModalConfirmDelete}
+                            footer={[
+                                <Button onClick={finishDeleteEdu} type="primary" htmlType="submit" className='bg-red-600'>Xóa</Button>
+                            ]}
+                        >
+                            <p>Bạn chắc chắn xóa ?</p>
+                        </Modal>
+                    </div>
+                    <div>
+                        {isModalEditEduOpen ? 
+                        (<Modal
+                            title="Sửa"
+                            open={isModalEditEduOpen}
+                            onCancel={cancelModalEditEdu}
+                            footer={null}
+                        >
+                            <Form
+                                labelCol={{ span: 8 }}
+                                wrapperCol={{ span: 16 }}
+                                style={{ maxWidth: 600 }}
+                                onFinish={(values :any) => finishEditEdu(values)}
+                            >
+                                <Form.Item
+                                    label="Trường"
+                                    name="schools"
+                                    initialValue={editEdu?.name?.split('*/')[0]}
+                                    rules={[{ required: true, message: 'Vui lòng nhập tên trường của bạn!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Ngành học"
+                                    name="majors"
+                                    initialValue={editEdu?.name?.split('*/')[1]}
+                                    rules={[{ required: true, message: 'Vui lòng nhập tên ngành của bạn!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+
+                                <Form.Item
+                                    name="isStillStudy"
+                                    valuePropName='checked'
+                                    initialValue={editEdu?.name?.split('*/')[3] == 'Hiện tại' ? true : false}
+                                    wrapperCol={{ offset: 8, span: 16 }}
+                                >
+                                    <Checkbox onChange={(e: any) => onChangeCheckBoxEdu(e.target.checked)} >Đang học</Checkbox>
+                                </Form.Item>
+                                <Form.Item
+                                    label="Từ"
+                                    name="start"
+                                    initialValue={moment(editEdu?.name?.split('*/')[2])}
+                                    rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
+                                >
+                                    <DatePicker picker="month" />
+                                </Form.Item>
+                                <Form.Item
+                                    label="Đến"
+                                    name="end"
+                                    initialValue={moment(editEdu.name.split('*/')[3] != 'Hiện tại' ? editEdu.name.split('*/')[3] : false)}
+                                    rules={[{ required: true, message: 'Vui lòng chọn mốc thời gian'}]}
+                                >
+                                    <DatePicker picker="month" disabled={disabledCheckboxEdu} />
+                                </Form.Item>
+                                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                                    <Button type="primary" htmlType="submit" className='bg-blue-600'>
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Modal>) : ''}
+                    </div>
+                    <div>
+                        <Card
+                            title={
+                                <>
+                                    <p className='font-bold text-3xl'>Học vấn</p>
+                                </>
+                            }
+                            extra={
+                                <>
+                                    <div className='mb-4 mt-2'>
+                                        <a onClick={() => showModalAddEdu()}><PlusCircleOutlined className='mr-4 text-2xl' /></a>
+                                    </div>
+                                </>
+                            }
+                            className='w-full mb-4 border-black'
+                        >
+                            {education?.data?.map((info: any) => {
+                                return (
+                                    <Card
+                                        key={info.id}
+                                        title={
+                                            <>
+                                                <p className='font-bold text-xl'>{info.name.split('*/')[0]}</p>
+                                            </>
+                                        }
+                                        extra={
+                                            <>
+                                                <a onClick={() => handleEditEdu(info)}><EditOutlined className='mr-8' /></a>
+                                                <a onClick={() => handleDeleteEdu(info.id)}><DeleteOutlined className='' /></a>
+                                            </>
+                                        }
+                                    >
+                                        <p className='my-3 text-base'>{info.name.split('*/')[1]}</p>
+                                        <p className='my-3 text-base'>{moment(info.name.split('*/')[2]).format('MM/YYYY') + ' - ' + (info.name.split('*/')[3] == 'Hiện tại' ? 'Hiện tại' : moment(info.name.split('*/')[3]).format('MM/YYYY')) }</p>
+                                    </Card>
+                                )
+                            })}
+                        </Card>
+                    </div>
                 </div >
             </>
         )
