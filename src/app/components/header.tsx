e568cab132ce02c0f5ba42a32fca99c446e1fcbf
dlir2404 <127393547+dlir2404@users.noreply.github.com>
2024-01-12 22:46:04 +0700

    fix

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
   const { data: levelData } = useGetListRecruitmentPost();
   const { data: businessData } = useGetListBusiness();
 
-  const items: MenuProps['items'] = [
+  const employerItems: MenuProps['items'] = [
     {
       label: 'All Jobs',
       key: 'All Job',
@@ -96,22 +96,87 @@ const Header = () => {
       children: [
         {
           label: (
-            <Link href="http://localhost:3000/pendingApplications">
+            <div onClick={() => router.push('/pendingApplications')}>
               Đang chờ
-            </Link>
+            </div>
           ),
           key: "application:1"
         },
         {
           label: (
-            <Link href="http://localhost:3000/acceptedApplications">
+            <div onClick={() => router.push('/acceptedApplications')}>
               Đã duyệt
-            </Link>
+            </div>
           ),
           key: "application:2"
         }
       ]
     }
+  ];
+
+  const employeeItems: MenuProps['items'] = [
+    {
+      label: 'All Jobs',
+      key: 'All Job',
+      children: [
+        {
+          label: "Việc làm IT theo kĩ năng",
+          key: "job by skills",
+          children: skillsData?.map((skill: any) => ({
+            label: skill.name,
+            key: skill.name,
+          }))
+        },
+        {
+          label: "Việc làm IT theo cấp bậc",
+          key: "job by Title",
+          // children: levelData?.filter((item: any) => item.level)?.map((role: any) => ({
+          //   label: role.level,
+          //   key: role.level,
+          // }))
+        },
+        {
+          label: "Việc làm IT theo công ty",
+          key: "job by company",
+          children: businessData?.map((role: any) => ({
+            label: role.businessName,
+            key: role.businessName,
+          }))
+        },
+        {
+          label: "Việc làm IT theo thành phố",
+          key: "job by cities",
+          children: provincesData?.data.map((item) => ({
+            label: item.name,
+            key: item.name,
+          })),
+        },
+      ],
+    },
+    {
+      label: 'Top Công ty IT',
+      key: 'app',
+      children: [
+        {
+          label: 'Công ty tốt nhất',
+          key: 'best companies',
+          children: [
+            {
+              label: 'Công ty 1',
+              key: 'company 1'
+            },
+            {
+              label: 'Công ty 2',
+              key: 'company 2'
+            }
+          ]
+        },
+        {
+          label: 'Review công ty',
+          key: 'review'
+        }
+      ]
+    },
   ];
 
   const verifyLogin = useQuery({
@@ -136,15 +201,16 @@ const Header = () => {
     }
 
   })
-  const handleLogout = async() => {
+
+  const handleLogout = async () => {
     try {
-      await http.getWithAutoRefreshToken('/api/auth/logout', {useAccessToken: true})
+      await http.getWithAutoRefreshToken('/api/auth/logout', { useAccessToken: true })
       sessionStorage.clear()
       localStorage.clear()
       setUser(null)
       message.success('Đăng xuất thành công')
       router.push('/')
-    }catch (error) {
+    } catch (error) {
       sessionStorage.clear()
       localStorage.clear()
       message.success('Đăng xuất thành công')
@@ -167,19 +233,19 @@ const Header = () => {
   )
 
   const employeeContent = (
-      <div className='min-w-14 cursor-pointer'>
-        <div className='py-2' onClick={() => router.push('/candidateProfile')}>
-          Hồ sơ
-        </div>
-        <div className='py-2' onClick={() => router.push('/manageApplication')}>
-          Việc làm của tôi
-        </div>
-        <div className='py-2'>
-          <button className='text-black' onClick={handleLogout} style={{color: 'black'}}>
-            Đăng xuất
-          </button>
-        </div>
+    <div className='min-w-14 cursor-pointer'>
+      <div className='py-2' onClick={() => router.push('/candidateProfile')}>
+        Hồ sơ
       </div>
+      <div className='py-2' onClick={() => router.push('/manageApplication')}>
+        Việc làm của tôi
+      </div>
+      <div className='py-2'>
+        <button className='text-black' onClick={handleLogout} style={{ color: 'black' }}>
+          Đăng xuất
+        </button>
+      </div>
+    </div>
   )
 
   const handleLoginClick = () => {
@@ -189,12 +255,12 @@ const Header = () => {
     router.push('/signup');
   };
 
-  const onNavigate: MenuProps['onClick'] = (e) => {
-    router.push(`/?key=${e.key}`)
-    setTimeout(() => {
-      window.location.reload()
-    }, 500)
-  };
+  // const onNavigate: MenuProps['onClick'] = (e) => {
+  //   router.push(`/?key=${e.key}`)
+  //   setTimeout(() => {
+  //     window.location.reload()
+  //   }, 500)
+  // };
 
 
   return (
@@ -206,7 +272,13 @@ const Header = () => {
         </div>
         <div className='ml-48 flex-1'>
           <div className='flex items-center gap-6 '>
-            <Menu className='bg-transparent min-w-[400px] text-xl text-[#a6a6a6]' mode="horizontal" selectedKeys={[]} items={items} onClick={onNavigate} />
+            {user?.id === 1 ?
+              (
+                <Menu className='bg-transparent min-w-[400px] text-xl text-[#a6a6a6]' mode="horizontal" selectedKeys={[]} items={employerItems} /*onClick={onNavigate}*/ />
+              ) :
+              (
+                <Menu className='bg-transparent min-w-[400px] text-xl text-[#a6a6a6]' mode="horizontal" selectedKeys={[]} items={employeeItems} /*onClick={onNavigate}*/ />
+              )}
           </div>
         </div>
         {user ? (
